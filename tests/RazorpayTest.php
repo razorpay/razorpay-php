fe7fd8354ca6c5376ae8e9534852949af198c8d1
Vivek Kumar <viveklakhera@gmail.com>
2016-10-11 18:23:36 +0530

    Add test case for checking notes class

--- tests/RazorpayTest.php
+++ tests/RazorpayTest.php
@@ -3,6 +3,7 @@
 namespace Razorpay\Tests;
 
 use Razorpay\Api\Api;
+use Razorpay\Api\Entity;
 use Razorpay\Api\Request;
 
 class RazorpayTest extends \PHPUnit_Framework_TestCase
@@ -27,6 +28,10 @@ class RazorpayTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(is_array($data->toArray()));
 
         $this->assertTrue(is_array($data['items']));
+
+        $payment = $data['items'][0];
+
+        $this->assertTrue(get_class($payment['notes']) === Entity::class);
     }
 
     public function testHeaders()
