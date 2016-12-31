acb4944d237fc38da0648ca5f8eb28b90dbf6c33
dharmendra <phpindia11@gmail.com>
2016-12-31 22:38:30 +0530

    Invoice Apis Support Added

---
+++ src/Invoice.php
@@ -0,0 +1,20 @@
+<?php
+namespace Razorpay\Api;
+
+class Invoice extends Entity
+{
+    /**
+     *  @param $id invoice
+     */
+    public function fetch($id = '')
+    {
+        return parent::fetch($id);
+    }
+
+    public function create($attributes = array())
+    {
+        return parent::create($attributes);
+    }    
+}
+
+?>
