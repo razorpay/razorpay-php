a842fb56efc0753aedbd5f6929d78120e992bc99
Pratik Kapasi <pratik.kapasi@razorpay.com>
2017-10-18 00:27:19 +0530

    [smart-collect] snake_case function - delimiter removed

--- src/Entity.php
+++ src/Entity.php
@@ -52,14 +52,14 @@ class Entity extends Resource implements ArrayableInterface
         return $className.'s/';
     }
 
-    protected function snake_case($input, $delimiter = '_')
+    protected function snake_case($input)
     {
         preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
         $ret = $matches[0];
         foreach ($ret as &$match) {
             $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
         }
-        return implode($delimiter, $ret);
+        return implode('_', $ret);
     }
 
     /**
