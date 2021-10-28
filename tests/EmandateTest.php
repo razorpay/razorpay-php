bff0465c850cd08c4f4b27f58b8747f282c3296d
ankitdas13 <ankit.das@razorpay.com>
2021-10-28 14:01:58 +0530

    change function name & remove static

--- tests/EmandateTest.php
+++ tests/EmandateTest.php
@@ -6,9 +6,9 @@ use Razorpay\Api\Request;
 
 class EmandateTest extends TestCase
 {
-    private static $customerId;
+    private $customerId = 'cust_IEfAt3ruD4OEzo';
 
-    private static $invoiceId;
+    private $invoiceId = 'inv_IEfS5mBV49bIQY';
 
     public function setUp()
     {
@@ -18,11 +18,9 @@ class EmandateTest extends TestCase
     /**
      * Create customer
      */
-    public function testcreate()
+    public function testCreateCustomerEmandate()
     {
-        $data = $this->api->customer->create(array('name' => 'Razorpay User 71', 'email' => 'customer71@razorpay.com', 'contact'=> 9999999999));
-        
-        self::$customerId = $data->id;
+        $data = $this->api->customer->create(array('name' => 'Razorpay User 71', 'email' => 'customer71@razorpay.com', 'contact'=> 9999999999, 'fail_existing'=>'0'));
 
         $this->assertTrue(is_array($data->toArray()));
 
@@ -32,9 +30,9 @@ class EmandateTest extends TestCase
     /**
      * Create Order
      */
-    public function testcreateOrder()
+    public function testCreateOrderEmandate()
     {
-        $data = $this->api->order->create(array('amount' => 100,'currency' => 'INR','method' => 'emandate','customer_id' => self::$customerId,'receipt' => 'Receipt No. '.time(), 'notes' => array('notes_key_1' => 'Beam me up Scotty','notes_key_2' => 'Engage'),'token' => array('auth_type' => 'netbanking','max_amount' => 9999900,'expire_at' => 4102444799,'notes' => array('notes_key_1' => 'Tea, Earl Grey, Hot','notes_key_2' => 'Tea, Earl Grey… decaf.'),'bank_account' => array('beneficiary_name' => 'Gaurav Kumar','account_number' => '1121431121541121','account_type' => 'savings','ifsc_code' => 'HDFC0000001'))));
+        $data = $this->api->order->create(array('amount' => 100,'currency' => 'INR','method' => 'emandate','customer_id' => $this->customerId,'receipt' => 'Receipt No. '.time(), 'notes' => array('notes_key_1' => 'Beam me up Scotty','notes_key_2' => 'Engage'),'token' => array('auth_type' => 'netbanking','max_amount' => 9999900,'expire_at' => 4102444799,'notes' => array('notes_key_1' => 'Tea, Earl Grey, Hot','notes_key_2' => 'Tea, Earl Grey… decaf.'),'bank_account' => array('beneficiary_name' => 'Gaurav Kumar','account_number' => '1121431121541121','account_type' => 'savings','ifsc_code' => 'HDFC0000001'))));
      
         $this->assertTrue(is_array($data->toArray()));
         
@@ -44,7 +42,7 @@ class EmandateTest extends TestCase
     /**
      * Create registration link
      */
-    public function testcreateSubscriptionRegistration()
+    public function testCreateSubscriptionRegistrationEmandate()
     {
         $data = $this->api->subscription->createSubscriptionRegistration(array('customer'=>array('name'=>'Gaurav Kumar','email'=>'gaurav.kumar@example.com','contact'=>'7000569565'),'type'=>'link','amount'=>100,'currency'=>'INR','description'=>'Registration Link for Gaurav Kumar','subscription_registration'=>array('method'=>'card','max_amount'=>'500','expire_at'=> strtotime("+1 month") ),'receipt'=>'Receipt No. '.time(),'email_notify'=>1,'sms_notify'=>1,'expire_by'=>strtotime("+1 month"),'notes' => array('note_key 1' => 'Beam me up Scotty','note_key 2' => 'Tea. Earl Gray. Hot.')));
 
@@ -52,31 +50,13 @@ class EmandateTest extends TestCase
 
         $this->assertTrue(in_array('customer',$data->toArray()));
     }
-
-    /**
-     * Create a Recurring Payment
-     */
-    public function testCreateRecurring()
-    {
-        $this->markTestSkipped(); 
-
-        $data = $this->api->payment->createRecurring(array('email'=>'gaurav.kumar@example.com','contact'=>'9123456789','amount'=>100,'currency'=>'INR','order_id'=>'order_I80LnO03SgjIzD','customer_id'=>'cust_HzoFfk52EjwayH','token'=>'token_I80QtqvWJrTzSl','recurring'=>'1','description'=>'Creating recurring payment for Gaurav Kumar'));
-  
-        $this->assertTrue(is_array($data->toArray()));
-
-        $this->assertTrue(in_array('razorpay_payment_id',$data->toArray()));
-    }
     
     /**
      * Send/Resend notifications
      */
-    public function testNotify()
+    public function testSendNotification()
     {
-        $data = $this->api->invoice->create(array ('type' => 'invoice', 'date' => time(), 'customer_id'=> self::$customerId, 'line_items'=>array(array("name"=> "Master Cloud Computing in 30 Days", "amount"=>10000, "currency" => "INR", "quantity"=> 1))));
-       
-        self::$invoiceId = $data->id;
-
-        $data = $this->api->invoice->fetch($data->id)->notifyBy('email');
+        $data = $this->api->invoice->fetch($this->invoiceId)->notifyBy('email');
 
         $this->assertTrue(is_array($data));
 
@@ -85,21 +65,9 @@ class EmandateTest extends TestCase
     }
 
     /**
-     * Cancel a registration link
-     */
-    public function testCancelRegistrationLink()
-    {
-        $data = $this->api->invoice->fetch(self::$invoiceId)->cancel();
-
-        $this->assertTrue(is_array($data->toArray()));
-        
-        $this->assertTrue(in_array('id',$data->toArray()));
-    }
-
-    /**
      * Fetch token by payment ID
      */
-    public function testFetchPaymentById()
+    public function testFetchTokenByPaymentId()
     {
        $payment = $this->api->payment->all();
 
@@ -113,25 +81,11 @@ class EmandateTest extends TestCase
     /**
      * Fetch tokens by customer id
      */
-    public function testfetchCustomer()
+    public function testFetchTokenByCustomerId()
     {
-       $data = $this->api->customer->fetch(self::$customerId)->tokens()->all();
+       $data = $this->api->customer->fetch($this->customerId)->tokens()->all();
 
        $this->assertTrue(is_array($data->toArray()));
-    }
-
-    /**
-     * Delete token
-     */
-    public function testDeleteToken()
-    {
-       $this->markTestSkipped(); 
-
-       $data = $this->api->customer->fetch($customerId)->tokens()->delete($tokenId);
-       
-       $this->assertTrue(is_array($data));
-
-       $this->assertTrue(in_array('deleted',$data));
     }
 
 }
--- tests/registerNachtest.php
+++ tests/registerNachtest.php
@@ -4,7 +4,7 @@ namespace Razorpay\Tests;
 
 use Razorpay\Api\Request;
 
-class paperNachTest extends TestCase
+class registerNachTest extends TestCase
 {
     private $customerId = 'cust_IEfAt3ruD4OEzo';
 
@@ -20,10 +20,10 @@ class paperNachTest extends TestCase
     /**
      * Create customer
      */
-    public function testCreateCustomerPaperNach()
+    public function testCreateCustomerForNachTest() 
     {
-        $data = $this->api->customer->create(array('name' => 'Razorpay User 72', 'email' => 'customer72@razorpay.com', 'fail_existing'=>'0'));
-
+        $data = $this->api->customer->create(array('name' => 'Razorpay User 21', 'email' => 'customer21@razorpay.com'));
+        
         $this->assertTrue(is_array($data->toArray()));
 
         $this->assertTrue(in_array('customer',$data->toArray()));
@@ -32,7 +32,7 @@ class paperNachTest extends TestCase
     /**
      * Create Order
      */
-    public function testCreateOrderPaperNach()
+    public function testCreateOrderForNachTest()
     {
         $data = $this->api->order->create(array('receipt' => '123', 'amount' => 100, 'currency' => 'INR', 'notes'=> array('key1'=> 'value3','key2'=> 'value2')));
 
@@ -42,7 +42,7 @@ class paperNachTest extends TestCase
     }
     
     /**
-     * Send notification
+     * Send/Resend notifications
      */
     public function testSendNotification()
     {
@@ -51,12 +51,11 @@ class paperNachTest extends TestCase
         $this->assertTrue(in_array('success',$data));
 
     }
-
-
+    
     /**
      * Create registration link
      */
-    public function testRegistrationLink()
+    public function testCreateSubscriptionRegistration()
     {
         $data = $this->api->subscription->createSubscriptionRegistration(array('customer' => array('name' => 'Gaurav Kumar','email' => 'gaurav.kumar@example.com','contact' => '9123456780'),'amount' => 0,'currency' => 'INR','type' => 'link','description' => '12 p.m. Meals','subscription_registration' => array('method' => 'nach','auth_type' => 'physical','bank_account' => array('beneficiary_name' => 'Gaurav Kumar','account_number' => '11214311215411','account_type' => 'savings','ifsc_code' => 'HDFC0001233'),'nach' => array('form_reference1' => 'Recurring Payment for Gaurav Kumar','form_reference2' => 'Method Paper NACH'),'expire_at' => 1636772800,'max_amount' => 50000),'receipt' => 'Receipt No. '.time(),'sms_notify' => 1,'email_notify' => 1,'expire_by' => 1636772800,'notes' => array('note_key 1' => 'Beam me up Scotty','note_key 2' => 'Tea. Earl Gray. Hot.')));
 
@@ -74,6 +73,19 @@ class paperNachTest extends TestCase
 
         $this->assertTrue(is_array($data->toArray()));
 
+        $this->assertTrue(is_array($data['items']));
+    }
+
+    /**
+     * Fetch tokens by customer id
+     */
+    public function testFetchTokenByCustomerId()
+    {
+       $data = $this->api->customer->fetch($this->customerId)->tokens()->all();
+
+       $this->assertTrue(is_array($data->toArray()));
+
+       $this->assertTrue(is_array($data['items']));
     }
 
     /**
@@ -104,19 +116,18 @@ class paperNachTest extends TestCase
 
         $this->assertTrue(in_array('id',$data->toArray()));
     }
-    
+
     /**
      * Create a Recurring Payment
      */
     public function testCreateRecurring()
     {
         $this->markTestSkipped(); 
-        
+
         $data = $this->api->payment->createRecurring(array('email'=>'gaurav.kumar@example.com','contact'=>'9123456789','amount'=>100,'currency'=>'INR','order_id'=>'order_I80LnO03SgjIzD','customer_id'=>'cust_HzoFfk52EjwayH','token'=>'token_I80QtqvWJrTzSl','recurring'=>'1','description'=>'Creating recurring payment for Gaurav Kumar'));
 
         $this->assertTrue(is_array($data->toArray()));
 
         $this->assertTrue(in_array('razorpay_payment_id',$data->toArray()));
     }
-
 }
