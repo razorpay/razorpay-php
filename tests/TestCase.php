28776a2977c3c5dbcbdf0cec18e8c41efa43515d
ankitdas13 <ankit.das@razorpay.com>
2021-10-29 11:36:28 +0530

    remove comment from testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,15 +7,12 @@ use Razorpay\Api\Request;
 
 class TestCase extends \PHPUnit_Framework_TestCase
 {
-    
+
     public function setUp()
     {
         $apiKey = 'rzp_test_1DP5mmOlF5G5ag';
         $apiSecret = 'thisissupersecret';
 
-        // $apiKey = 'rzp_test_G8lBRRcxH1t9Bd';
-        // $apiSecret = 'Iwm6wk1Wy5AnB7ahSIpfJYxP';
-        
         $this->api = new Api( $apiKey, $apiSecret);
     }
 }
