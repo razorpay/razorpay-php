97c659b0a45ac7f74a0215b9aa3d9978d65c8a29
Ankit Das <89454448+ankitdas13@users.noreply.github.com>
2023-04-11 10:54:48 +0530

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ use Razorpay\Api\Request;
 use PHPUnit\Framework\TestCase as PhpUnitTest;
 
 $key = $_ENV["RAZORPAY_API_KEY"];
-$secret = $_ENV["RAZORPAY_API_KEY"];
+$secret = $_ENV["RAZORPAY_API_SECRET"];
 
 
 class TestCase extends PhpUnitTest
