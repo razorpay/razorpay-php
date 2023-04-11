4d28312765fb34d8589321a27d34bec46dc30bad
Ankit Das <89454448+ankitdas13@users.noreply.github.com>
2023-04-11 12:50:26 +0530

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace Razorpay\Tests;
+require_once realpath(__DIR__ . "/vendor/autoload.php");
 
 use Razorpay\Api\Api;
 use Razorpay\Api\Request;
 use PHPUnit\Framework\TestCase as PhpUnitTest;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $key = $_ENV["RAZORPAY_API_KEY"];
 $secret = $_ENV["RAZORPAY_API_SECRET"];
