04846b52a69a86691058b05ca33f0da38dccf74a
Ankit Das <89454448+ankitdas13@users.noreply.github.com>
2023-04-11 19:20:03 +0530

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,17 +11,14 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$key = $_ENV["RAZORPAY_API_KEY"];
-$secret = $_ENV["RAZORPAY_API_SECRET"];
-
 
 class TestCase extends PhpUnitTest
 {
     
     public function setUp(): void
     {
-        $key = $_ENV["RAZORPAY_API_KEY"];
-        $secret = $_ENV["RAZORPAY_API_SECRET"];
+        $key = getenv("RAZORPAY_API_KEY");
+        $secret = getenv("RAZORPAY_API_SECRET");
         
         $this->api = new Api( $apiKey, $apiSecret);
     }
