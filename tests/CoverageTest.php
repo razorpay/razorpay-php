0013ff9d8b96394273d91795342d4d09d47ded95
ankitdas13 <ankit.das@razorpay.com>
2023-04-12 13:36:31 +0530

    update cases

--- tests/CoverageTest.php
+++ tests/CoverageTest.php
@@ -7,6 +7,17 @@ use Razorpay\Api\Request;
 class CoverageTest extends TestCase
 {
     /**
+     * @covers \Razorpay\Api\Addon::fetch
+     * @covers \Razorpay\Api\Addon::fetchAll
+     */
+    public function testAddonCoverage(){
+      $addon = new AddonTest();
+      $addon->setUp();
+      $addon->testFetchSubscriptionLink();
+      $addon->testFetchAllAddon();
+    }
+
+    /**
      * @covers \Razorpay\Api\Customer::create
      * @covers \Razorpay\Api\Customer::edit
      * @covers \Razorpay\Api\Customer::all
@@ -21,4 +32,43 @@ class CoverageTest extends TestCase
       $customer->testFetchCustomer();
     }
 
+    /**
+     * @covers \Razorpay\Api\Card::fetch
+     */   
+    public function testCardCoverage(){
+      $card = new CardTest();
+      $card->setup();
+      $card->testFetchCard();
+    }
+
+    /**
+     * @covers \Razorpay\Api\FundAccount::create
+     * @covers \Razorpay\Api\FundAccount::all
+     */
+    public function testFundCoverage(){
+      $fund = new FundTest();
+      $fund->setup();
+      $fund->testCreateFundAccount();
+      $fund->testCreateOrder();
+    }
+
+      /**
+       * @covers \Razorpay\Api\Invoice::create
+       * @covers \Razorpay\Api\Invoice::all
+       * @covers \Razorpay\Api\Invoice::edit
+       * @covers \Razorpay\Api\Invoice::notifyBy
+       * @covers \Razorpay\Api\Invoice::delete
+       * @covers \Razorpay\Api\Invoice::cancel
+       * @uses \Razorpay\Api\Invoice::fetch
+       */
+      public function testInvoiceCoverage(){
+        $invoice = new InvoiceTest();
+        $invoice->setup();
+        $invoice->testCreateInvoice();
+        $invoice->testFetchAllInvoice();
+        $invoice->testUpdateInvoice();
+        $invoice->testInvoiceIssue();
+        $invoice->testDeleteInvoice();
+        $invoice->testCancelInvoice();
+      }   
 }
