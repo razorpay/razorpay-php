b78bc829e9a29086a326fa076bab3737561177f9
ankitdas13 <ankit.das@razorpay.com>
2023-04-14 15:58:40 +0530

    handle library

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,17 +8,19 @@ use Razorpay\Api\Request;
 use PHPUnit\Framework\TestCase as PhpUnitTest;
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
+if (class_exists('Dotenv'))
+{
+ $dotenv = Dotenv::createImmutable(__DIR__);
+ $dotenv->load();
+}
 
 class TestCase extends PhpUnitTest
 {
     
     public function setUp(): void
     {
-        $apiKey = getenv("RAZORPAY_API_KEY");
-        $apiSecret = getenv("RAZORPAY_API_SECRET");
+        $apiKey = getenv("RAZORPAY_API_KEY") ? getenv("RAZORPAY_API_KEY") : "";
+        $apiSecret = getenv("RAZORPAY_API_SECRET") ? getenv("RAZORPAY_API_SECRET") : "";
         
         $this->api = new Api( $apiKey, $apiSecret);
     }
