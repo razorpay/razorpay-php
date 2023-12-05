8661edfc08aea312f881e569221687b5a300696c
ankitdas13 <ankit.das@razorpay.com>
2023-12-05 11:45:05 +0530

    added new api's

---
+++ src/Document.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Razorpay\Api;
+
+class Document extends Entity
+{
+
+    public function create($attributes = array())
+    {
+        return parent::create($attributes);
+    }
+
+    public function fetch($id)
+    {
+        return parent::fetch($id);
+    }
+
+}
