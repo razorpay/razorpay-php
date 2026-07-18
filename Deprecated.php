ab127bcd3d3061ab44b12b986f5b77a3f8c39828
Sai Asish Y <say.apm35@gmail.com>
2026-07-18 05:08:50 -0700

    Guard REQUESTS_SILENCE_PSR0_DEPRECATIONS define in Deprecated.php
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- Deprecated.php
+++ Deprecated.php
@@ -11,7 +11,10 @@
  *
  * @deprecated 2.0.4 Use the PSR-4 class names instead.
  */
-define("REQUESTS_SILENCE_PSR0_DEPRECATIONS",true);
+if (!defined('REQUESTS_SILENCE_PSR0_DEPRECATIONS'))
+{
+    define('REQUESTS_SILENCE_PSR0_DEPRECATIONS', true);
+}
 
 if (class_exists('WpOrg\Requests\Autoload') === false) {
 	require_once __DIR__. 'libs/Requests-2.0.4/src/Autoload.php';
